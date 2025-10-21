00b11c111ede9a37389352b8e52858bd359ef5df
Viacheslav Kaliak <trinidaa@gmial.com>
2025-10-21 15:57:40 +0200

    solution_1.8

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 module "resource_group_storage" {
-  source  = "github.com/trinidaa/terraform-azurerm-modules//resource-group/azurerm"
-
+  source = "github.com/trinidaa/terraform-azurerm-modules//resource-group/azurerm"
   version = "v1.0.0"
 }
