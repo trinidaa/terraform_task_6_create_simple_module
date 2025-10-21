67eb561c508a50bb48491a6fb00abcf31eb65cd7
Viacheslav Kaliak <trinidaa@gmial.com>
2025-10-22 00:01:35 +0200

    solution_1.0.0

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.70.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
 module "resource_group_storage" {
   source  = "trinidaa/terraform-azurerm-resource_group_storage"
   version = "1.0.0"
